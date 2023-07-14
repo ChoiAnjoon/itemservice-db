01b51d8fb96de32e92b6f5a1c7e5760dea188e47
choi <dkswnsl12@naver.com>
2023-07-14 20:56:42 +0900

    MyBatis 코드 보충 설명

--- src/main/java/hello/itemservice/repository/mybatis/ItemMapper.java
+++ src/main/java/hello/itemservice/repository/mybatis/ItemMapper.java
@@ -9,6 +9,9 @@ import org.apache.ibatis.annotations.Param;
 import java.util.List;
 import java.util.Optional;
 
+// 1. 에플리케이션 로딩 시점에 MyBatis 스프링 연동 모듈은 @Mapper가 붙어있는 인터페이스를 찾는다.
+// 2. 해당 인터페이스가 발견되면 동적 프록시 기술을 사용해서 ItemMapper 인터페이스의 구현체를 만든다.
+// 3. 생성된 구현체를 스프링 빈으로 자동 등록해준다.
 @Mapper
 public interface ItemMapper {
 
--- src/main/java/hello/itemservice/repository/mybatis/MyBatisItemRepository.java
+++ src/main/java/hello/itemservice/repository/mybatis/MyBatisItemRepository.java
@@ -17,6 +17,11 @@ import java.util.Optional;
 public class MyBatisItemRepository implements ItemRepository {
 
     private final ItemMapper itemMapper;
+    // 1. 에플리케이션 로딩 시점에 MyBatis 스프링 연동 모듈은 @Mapper가 붙어있는 인터페이스를 찾는다.
+    // 2. 해당 인터페이스가 발견되면 동적 프록시 기술을 사용해서 ItemMapper 인터페이스의 구현체를 만든다.
+    // 3. 생성된 구현체를 스프링 빈으로 자동 등록해준다.
+    // 스프링빈에 itemMapper 클래스가 자동 등록 되있으므로 MyBatisConfig에서는 자동으로 @Autowired로 주입받아서 Repository로 전달해줌
+    // 이 예제는 config를 사용했지만 현재 @Repository 자동 컴포넌트 스캔으로도 지금 이 자체로 스프링빈에 등록된 itemMapper를 (@Autowired(생략)) 주입받을 수 있다.
 
     @Override
     public Item save(Item item) {
